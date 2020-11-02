f9df25e2c4a769f542b535cd70b3dadbc648d750
Shintaro Abe <dictav@gmail.com>
2020-11-03 02:09:00 +0900

    update backend/main.go

--- backend/main.go
+++ backend/main.go
@@ -21,6 +21,9 @@ func main() {
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		rw.Write([]byte("hello"))
+
+
+		rw.Write([]byte("hello"))
 	})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
